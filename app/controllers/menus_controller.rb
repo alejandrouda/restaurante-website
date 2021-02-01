06223586cf3059b5ad22d337f729b4cedb146439
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2021-02-01 14:33:58 +0100

    refactoring menu controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -9,23 +9,30 @@ class MenusController < ApplicationController
   end
 
   def show
-    @meals = Meal.all
-    @meal_all = Meal.where(menu_id: @menu.id)
-    @meal_glutenfree = Filter.find_by(filter_title: 'glutenfree').meals.where(menu_id: @menu.id)
-    @meal_unlactosed = Filter.find_by(filter_title: 'unlactosed').meals.where(menu_id: @menu.id)
-    @meal_vegan = Filter.find_by(filter_title: 'vegan').meals.where(menu_id: @menu.id)
-    @meal_vegetarian = Filter.find_by(filter_title: 'vegetarian').meals.where(menu_id: @menu.id)
-    @current_drinks = Drink.where(menu_id: @menu.id)
     @current_restaurante = Restaurante.find(params[:restaurante_id].to_i)
     @meal_categories = MealCategory.all
     @drink_categories = DrinkCategory.all
     @filters = Filter.all
     @allergens = Allergen.all
+    @meals = Meal.all
+    @meal_all = Meal.where(menu_id: @menu.id)
+    @meal_glutenfree = meal_filter('glutenfree')
+    @meal_unlactosed = meal_filter('unlactosed')
+    @meal_vegan = meal_filter('vegan')
+    @meal_vegetarian = meal_filter('vegetarian')
+    @current_drinks = Drink.where(menu_id: @menu.id)
   end
 
   private
 
+
+
   def set_params
     @menu = Menu.find(params[:id])
   end
+
+  def meal_filter(filter)
+    Filter.find_by(filter_title: filter).meals.where(menu_id: @menu.id)
+  end
+
 end
